b689c49ffb375360ea390297bf35265e4479ecad
wangziqiang <wziqiangcn@gmail.com>
2015-03-25 21:59:12 +0800

    自定义ProgressBar步骤

---
+++ app/src/main/java/com/sf/wzq/view/NumberProgressBar.java
@@ -0,0 +1,19 @@
+package com.sf.wzq.view;
+
+
+/**
+ * Created by sf on 2015/3/25.<br>
+ * <ul>本项目学习点：
+ * <li>1，从零开始学习一个自定义控件的思考步骤.</li>
+ * <li>2，本项目基本流程和相关知识点.</li>
+ * </ul>
+ * 基本流程:
+ * <ul>1,初始化
+ * <li>属性参数</li>
+ * <li>画笔</li>
+ * </ul>
+ * <ul>2,onMeasure</ul>
+ * <ul>3,onDraw</ul>
+ */
+public class NumberProgressBar {
+}

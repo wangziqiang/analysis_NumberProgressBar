97ef0ce9b1a951ad4113b82b9e86ac7acdab0cdd
wangziqiang <wziqiangcn@gmail.com>
2015-04-07 17:58:40 +0800

    add demo text

--- app/src/main/java/com/sf/wzq/analysis_numberprogressbar/MainActivity.java
+++ app/src/main/java/com/sf/wzq/analysis_numberprogressbar/MainActivity.java
@@ -2,16 +2,34 @@ package com.sf.wzq.analysis_numberprogressbar;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.sf.wzq.view.NumberProgressBar;
+
 
 public class MainActivity extends Activity {
-
+    String tag = "NumberProgressBar";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final NumberProgressBar progressBar = (NumberProgressBar) findViewById(R.id.progress1);
+        CountDownTimer timer = new CountDownTimer(5000,50) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                progressBar.increaseProgressBy(1);
+                Log.i(tag,"millisUntilFinished = "+millisUntilFinished/1000);
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
+        timer.start();
     }
 
 
